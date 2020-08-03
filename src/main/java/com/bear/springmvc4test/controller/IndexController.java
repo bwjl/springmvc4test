a3e3391fcb8650e2986da9e5543810a2e2f99a72
bear <593788753@qq.com>
2020-08-04 00:59:29 +0800

    springmvc4.x

---
+++ src/main/java/com/bear/springmvc4test/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.bear.springmvc4test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/8/4 上午12:33
+ * Description:
+ */
+
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+    @GetMapping("index")
+    public void index()
+    {
+        System.out.println("index");
+    }
+}
